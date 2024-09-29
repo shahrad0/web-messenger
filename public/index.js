a35054486299faff905e97d737dd363b87c903ea
shahrad0 <hunterlogan132@gmail.com>
2024-09-29 18:50:11 +0400

    kinda finished pre written text part  (still need more features) but it works and everything else is fine and functional

--- public/index.js
+++ public/index.js
@@ -8,13 +8,13 @@ document.addEventListener('DOMContentLoaded', function() {
   if (!authorized) window.location.href = '/Authorize/'
 });
 let socket = io();
-const form = document.getElementById('form');
-const input = document.getElementById('input');
+const form     = document.getElementById('form');
+const input    = document.getElementById('input');
 const messages = document.getElementById("messages")
+const menu     = document.getElementById("menu")
 socket.on('chat message', (msg) => {
   // Append the message to the chat without refreshing the page
   messages.innerHTML += messageTemplate(msg.message, msg.username, msg.profileImage,msg.id);
-  console.log(msg.id)
   // Scroll to the bottom after appending a new message
   setTimeout(() => {scrollToBottom("messages")},10)
 })
@@ -23,7 +23,7 @@ socket.on('chat message', (msg) => {
 const divider = document.getElementById("divider")
 divider.addEventListener("mousedown",()=>{
   divider.addEventListener("mousemove",()=>{
-    console.log("a")
+
   })
 })
 //end divider
@@ -138,7 +138,6 @@ document.addEventListener("click", function(event) {
             </div>
           </div>
         `;
-
         const closeBlackScreen = document.getElementById("close-black-screen");
         closeBlackScreen.addEventListener("click", () => {
           blackScreen.style.display = `none`;
@@ -161,13 +160,9 @@ document.getElementById("turn-off").addEventListener("click",()=>{
       document.getElementById("off").style.display="none"
       toggleOff = false 
   })})
-// end turn off button 
-// shortcut for turn off
 let altPressed
 document.addEventListener("keydown", function (event) {
-  if (event.keyCode === 18) {
-    altPressed = true; // Alt key is pressed
-  }
+  if (event.keyCode === 18)  altPressed = true; // Alt key is pressed
 
   if (altPressed && event.keyCode === 190) { // Alt + .
     if (toggleOff) {
@@ -185,14 +180,13 @@ document.addEventListener("keydown", function (event) {
     })
   }
 }});
-
 // Track when Alt is released
 document.addEventListener("keyup", function (event) {
   if (event.keyCode === 18) {
     altPressed = false; // Reset Alt key state when released
   }
 });
-// end shortcut for turn off
+// end turn off button 
 
 const moreButton = document.getElementById("more")
 const moreMenu = document.getElementById("more-menu")
@@ -210,119 +204,143 @@ moreButton.addEventListener("click",()=>{
 // add hover 
 
 // setting 
-// settingButton = document.getElementById("setting-button")
-// settingButton.addEventListener("click",()=>{
-//     const id = localStorage.getItem(`userId`)
-//     fetch(`/user-details?id=${encodeURIComponent(id)}`)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(`Error: ${response.status} ${response.statusText}`);
-//       }
-//       return response.json();  // Attempt to parse JSON only if the response is okay
-//     })
-//     .then(user => {
-//       if (user) {
-//         // Display the user details (e.g., in a modal or a separate section)
-//         blackScreen.style.display = 'block';
-//         blackScreen.innerHTML = `
-//         <div class="user-profile-detail">
-//           <div class="user-info">
-//             <p>Profile</p>
-//             <button id="edit-profile"><svg  viewBox="0 0 24 24" id="_24x24_On_Light_Edit" data-name="24x24/On Light/Edit" xmlns="http://www.w3.org/2000/svg"><rect id="view-box" width="24" height="24" fill="none"/><path id="Shape" d="M.75,17.5A.751.751,0,0,1,0,16.75V12.569a.755.755,0,0,1,.22-.53L11.461.8a2.72,2.72,0,0,1,3.848,0L16.7,2.191a2.72,2.72,0,0,1,0,3.848L5.462,17.28a.747.747,0,0,1-.531.22ZM1.5,12.879V16h3.12l7.91-7.91L9.41,4.97ZM13.591,7.03l2.051-2.051a1.223,1.223,0,0,0,0-1.727L14.249,1.858a1.222,1.222,0,0,0-1.727,0L10.47,3.91Z" transform="translate(3.25 3.25)" fill="#141124"/></svg></button>
-//             <button id="close-black-screen"><svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 460.775 460.775" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"/><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/><g id="SVGRepo_iconCarrier"> <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/> </g></svg></button>
-//           </div>
-//           <div class="user-info-container">
-//             <img class="user-image" src="/uploads/${user.profile_image}" alt="">
-//             <div style="width: 100%;">
-//               <p class="user-detail">${user.username}</p>
-//               <p class="user-detail">User ID: ${user.id}</p>
-//               <p class="user-detail">User Role: ${user.role}</p>
-//             </div>
-//           </div>
-//         `;
+function settingButtonSetup(){
+  settingButton = document.getElementById("setting-button")
+  settingButton.addEventListener("click",()=>{
+      const id = localStorage.getItem(`userId`)
+      fetch(`/user-details?id=${encodeURIComponent(id)}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        return response.json();  // Attempt to parse JSON only if the response is okay
+      })
+      .then(user => {
+        if (user) {
+          // Display the user details (e.g., in a modal or a separate section)
+          blackScreen.style.display = 'block';
+          blackScreen.innerHTML = `
+          <div class="user-profile-detail">
+            <div class="user-info">
+              <p>Profile</p>
+              <button id="edit-profile"><svg  viewBox="0 0 24 24" id="_24x24_On_Light_Edit" data-name="24x24/On Light/Edit" xmlns="http://www.w3.org/2000/svg"><rect id="view-box" width="24" height="24" fill="none"/><path id="Shape" d="M.75,17.5A.751.751,0,0,1,0,16.75V12.569a.755.755,0,0,1,.22-.53L11.461.8a2.72,2.72,0,0,1,3.848,0L16.7,2.191a2.72,2.72,0,0,1,0,3.848L5.462,17.28a.747.747,0,0,1-.531.22ZM1.5,12.879V16h3.12l7.91-7.91L9.41,4.97ZM13.591,7.03l2.051-2.051a1.223,1.223,0,0,0,0-1.727L14.249,1.858a1.222,1.222,0,0,0-1.727,0L10.47,3.91Z" transform="translate(3.25 3.25)" fill="#141124"/></svg></button>
+              <button id="close-black-screen"><svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 460.775 460.775" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"/><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/><g id="SVGRepo_iconCarrier"> <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/> </g></svg></button>
+            </div>
+            <div class="user-info-container">
+              <img class="user-image" src="/uploads/${user.profile_image}" alt="">
+              <div style="width: 100%;">
+                <p class="user-detail">${user.username}</p>
+                <p class="user-detail">User ID: ${user.id}</p>
+                <p class="user-detail">User Role: ${user.role}</p>
+              </div>
+            </div>
+          `;
+          const closeBlackScreen = document.getElementById("close-black-screen");
+          closeBlackScreen.addEventListener("click", () => {
+            blackScreen.style.display = `none`;
+          });
+          // edit profile 
+          const editProfile = document.getElementById("edit-profile")
+          editProfile.addEventListener("click",()=>{
+            blackScreen.innerHTML = `
+            <form id="editProfileForm" action="/update-profile" method="POST" enctype="multipart/form-data">
+              <input type="hidden" name="userId"   value="${user.id}" />
+              <input type="text"   name="username" id="new-username"  placeholder="Enter new name" required />
+              <input type="file"   name="profile_image" />
+              <button  type="submit">Update Profile</button>
+            </form>`
+          })
+  
+          // end edir profile 
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching user details:', error);
+      });
+    })
+}
+settingButtonSetup()
+// end setting 
 
-//         const closeBlackScreen = document.getElementById("close-black-screen");
-//         closeBlackScreen.addEventListener("click", () => {
-//           blackScreen.style.display = `none`;
-//         });
-//         // edit profile 
-
-//         const editProfile = document.getElementById("edit-profile")
-//         editProfile.addEventListener("click",()=>{
-//           blackScreen.innerHTML = `
-//           <form id="editProfileForm" action="/update-profile" method="POST" enctype="multipart/form-data">
-//             <input type="hidden" name="userId"   value="${user.id}" />
-//             <input type="text"   name="username" id="new-username"  placeholder="Enter new name" required />
-//             <input type="file"   name="profile_image" />
-//             <button  type="submit">Update Profile</button>
-//           </form>`
-//         })
-
-//         // end edir profile 
-//       }
-//     })
-//     .catch(error => {
-//       console.error('Error fetching user details:', error);
-//     });
-//   })
-
-
-// pre written text
-// function charToString(charArray){
-//   for(int)
-// }
-
-const preWritten = document.getElementById("pre-written") 
-preWritten.addEventListener("click",()=>{
+// pre written text  
+// for menu
+const preWrittenMenu = document.getElementById("pre-written")
+preWrittenMenu.addEventListener("click",()=>{
+  menu.innerHTML = `
+      <div id="pre-written-text-menu-container">
+      <div> 
+        <form action="" id="submit-pre-written-text" >
+          <div class="note"> <button id="close-menu" type="button"><svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 460.775 460.775" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"/><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/><g id="SVGRepo_iconCarrier"> <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/> </g></svg></button>
+          <p>Add more words at once by seprating words with ","</p>
+          </div>
+          <input  id="submit-text" type="text" placeholder="submit-text">
+          <button id="submit-pre-written-text-button"> add </button>
+        </form>
+      </div>
+      <div id="pre-written-text-container">
+      </div>
+    </div>
+  `
   moreMenu.style.opacity = `0`  
   moreMenuToggle = false
+  updatePreWrittenText(input)
+  // actual main functionality
+  document.getElementById("submit-pre-written-text").addEventListener(`submit`,function(e){
+    e.preventDefault();
+    let preWrittenTextInput = document.getElementById("submit-text") 
+    let preWrittenText = preWrittenTextInput.value
+    let tempArray = []
+    let tempText = ''
+    for (let i = 0;i < preWrittenText.length;i++){
+      if(preWrittenText[i] == `,` || i > preWrittenText.length-2){
+        if(i > preWrittenText.length-2){
+          tempText+= preWrittenText[i]
+          tempArray.push(tempText)
+          tempText= ''
+          preWrittenTextInput.value = ''
+          let savedPreWrittenText = JSON.parse(localStorage.getItem('preWrittenText'))
+          if (savedPreWrittenText){
+            newSavedPreWrittenText = tempArray.concat(savedPreWrittenText)
+            localStorage.setItem('preWrittenText', JSON.stringify(newSavedPreWrittenText))
+            updatePreWrittenText(input)
+          }
+          else localStorage.setItem('preWrittenText', JSON.stringify(tempArray))
+        }
+        else{
+        tempArray.push(tempText)
+        tempText= ''
+      }
+      }
+      else if (preWrittenText[i] != `,`  ) tempText+= preWrittenText[i]
+    }
+  })
+  // closing menu button 
+  const closeMenu = document.getElementById("close-menu")
+  closeMenu.addEventListener(`click`,()=>{
+    menu.innerHTML = `
+      <div>
+        <button id="setting-button">
+          <svg  xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" >    <path d="M47.16,21.221l-5.91-0.966c-0.346-1.186-0.819-2.326-1.411-3.405l3.45-4.917c0.279-0.397,0.231-0.938-0.112-1.282 l-3.889-3.887c-0.347-0.346-0.893-0.391-1.291-0.104l-4.843,3.481c-1.089-0.602-2.239-1.08-3.432-1.427l-1.031-5.886 C28.607,2.35,28.192,2,27.706,2h-5.5c-0.49,0-0.908,0.355-0.987,0.839l-0.956,5.854c-1.2,0.345-2.352,0.818-3.437,1.412l-4.83-3.45 c-0.399-0.285-0.942-0.239-1.289,0.106L6.82,10.648c-0.343,0.343-0.391,0.883-0.112,1.28l3.399,4.863 c-0.605,1.095-1.087,2.254-1.438,3.46l-5.831,0.971c-0.482,0.08-0.836,0.498-0.836,0.986v5.5c0,0.485,0.348,0.9,0.825,0.985 l5.831,1.034c0.349,1.203,0.831,2.362,1.438,3.46l-3.441,4.813c-0.284,0.397-0.239,0.942,0.106,1.289l3.888,3.891 c0.343,0.343,0.884,0.391,1.281,0.112l4.87-3.411c1.093,0.601,2.248,1.078,3.445,1.424l0.976,5.861C21.3,47.647,21.717,48,22.206,48 h5.5c0.485,0,0.9-0.348,0.984-0.825l1.045-5.89c1.199-0.353,2.348-0.833,3.43-1.435l4.905,3.441 c0.398,0.281,0.938,0.232,1.282-0.111l3.888-3.891c0.346-0.347,0.391-0.894,0.104-1.292l-3.498-4.857 c0.593-1.08,1.064-2.222,1.407-3.408l5.918-1.039c0.479-0.084,0.827-0.5,0.827-0.985v-5.5C47.999,21.718,47.644,21.3,47.16,21.221z M25,32c-3.866,0-7-3.134-7-7c0-3.866,3.134-7,7-7s7,3.134,7,7C32,28.866,28.866,32,25,32z"/></svg>
+        </button>
+      </div>`
+    settingButtonSetup()
+  })
+})
+function updatePreWrittenText(chatInput){
   let savedPreWrittenText = JSON.parse(localStorage.getItem('preWrittenText'))
+  document.getElementById("pre-written-text-container").innerHTML = ''
   if (savedPreWrittenText){
     savedPreWrittenText.forEach(element => {
       document.getElementById("pre-written-text-container").innerHTML += `          
-      <div class="pre-written-text">
-      ${element}
-      </div>`
-      
+      <div class="pre-written-text">${element} </div>`
     });
   }
-})
-document.getElementById("submit-pre-written-text").addEventListener(`submit`,function(e){
-  e.preventDefault();
-  let preWrittenTextInput = document.getElementById("submit-text") 
-  let preWrittenText = preWrittenTextInput.value
-  let tempArray = []
-  let tempText = ''
-  for (let i = 0;i < preWrittenText.length;i++){
-    if(preWrittenText[i] == `,` || i > preWrittenText.length-2){
-      if(i > preWrittenText.length-2){
-        tempText+= preWrittenText[i]
-        tempArray.push(tempText)
-        tempText= ''
-        preWrittenTextInput.value = ''
-        console.log(tempArray)
-        let savedPreWrittenText = JSON.parse(localStorage.getItem('preWrittenText'))
-        if (savedPreWrittenText){
-          newSavedPreWrittenText = tempArray.concat(savedPreWrittenText)
-          localStorage.setItem('preWrittenText', JSON.stringify(newSavedPreWrittenText))
+  const preWrittenText = document.querySelectorAll(".pre-written-text")
+  preWrittenText.forEach(element => {
+    element.addEventListener(`click`,()=>{
+      chatInput.value += element.innerHTML
 
-        }else{
-          localStorage.setItem('preWrittenText', JSON.stringify(tempArray))
-
-
-        }
-      }
-      else{
-      console.log(tempText)
-      tempArray.push(tempText)
-      tempText= ''
-      console.log(tempArray)
-    }
-    }
-    else if (preWrittenText[i] != `,`  ){
-      tempText+= preWrittenText[i]
-      console.log(tempText)
-    }
-  }
-})
+    })
+  });
+}
 // end pre written text
